693bf434fe630a0aae516ddb4baab407f6566539
AbuMdNayeemKhan <abumdnayeemkhan@gmail.com>
2026-02-20 22:22:30 +0600

    header section dynamic running.

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+// Link or enqueue Script and style 
+function projukti_theme_scripts(){
+    // Google fonts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sen:wght@400..800&display=swap', array(), null);
+
+    // Custom CSS link
+    wp_enqueue_style('custom-styles', get_template_directory_uri() . '/assets/css/style.css');
+
+    // Custom Responsive CSS link
+    wp_enqueue_style('custom-responsive-style', get_template_directory_uri() . '/assets/css/responsive.css');
+
+    // Slick Slider CSS ad
+    wp_enqueue_style('slick-slider', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css', array(), '1.9.0');
+
+    // Style CSS
+    wp_enqueue_style('style-css', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('style-css', get_stylesheet_uri());
+
+    // Box Icon
+    wp_enqueue_style('box-icon', 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css', array(), '2.1.4');
+
+    // add jQuery
+    wp_enqueue_script('jquery');
+
+    // Slick JS
+    wp_enqueue_script('slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', array('jquery'), '1.9.0', true);
+
+    // Main Js
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), true);
+}
+add_action('wp_enqueue_scripts','projukti_theme_scripts');
+
+function projukti_theme_custom_logo() {
+	$defaults = array(
+		'height'               => 30,
+		'width'                => 80,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'header-text'          => array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' => false, 
+	);
+	add_theme_support( 'custom-logo', $defaults );
+}
+add_action('after_setup_theme', 'projukti_theme_custom_logo');
+?>
---
+++ header.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('title');?> || <?php bloginfo('discription');?></title>
+    <?php wp_head();?>
+</head>
+<body <?php body_class();?>>
+    <header>
+        <div class="container">
+            <div class="header-wrapper">
+                <!----- logo ----->
+                <div class="logo">
+                    <?php if(has_custom_logo()) : ?>
+                    <?php the_custom_logo(); ?>
+                    <?php else : ?>
+                        <a href="<?php echo esc_url(home_url('/')); ?>">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo">
+                        </a>
+                    <?php endif; ?>
+                </div>
+                <!----- main-menu and button ----->
+                <div class="menu-button-wrapper">
+                    <nav class="main-menu">
+                        <ul>
+                            <li><a class="active" href="#">Home</a></li>
+                            <li><a href="#">Courses</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">Web Design</a>
+                                        <ul>
+                                            <li><a href="#">HTML/CSS</a></li>
+                                            <li><a href="#">JavaScript</a></li>
+                                            <li><a href="#">Frameworks</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">Web Development</a>
+                                        <ul>
+                                            <li><a href="#">Frontend</a></li>
+                                            <li><a href="#">Backend</a></li>
+                                            <li><a href="#">Full Stack</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Graphic Design</a></li>
+                                    <li><a href="#">UI/UX Design</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Reviews</a></li>
+                        </ul>
+
+                    </nav>
+
+                    <!----- sign-up-btn ----->
+                    <div class="button btn-black">
+                        <a href="#">Sign Up</a>
+                    </div>
+
+
+                    <!----- phone menu ----->
+                    <div class="menu-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                        </svg>
+                    </div>
+                </div>
+
+                <!----- menu item for phone ----->
+                <div  id="navPhone" class="menu-item-phone">
+
+                    <!----- logo ----->
+                    <div class="logo-div">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo">
+                            </a>
+                        </div>
+
+                        <div class="x-icon menu-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                <line x1="18" y1="6" x2="6" y2="18" stroke-linecap="round" />
+                                <line x1="6" y1="6" x2="18" y2="18" stroke-linecap="round" />
+                            </svg>
+                        </div>
+                    </div>
+                    
+
+
+                    <div class="menu-div">
+                        <nav class="main-menu-phone">
+                            <ul>
+                                <li><a class="active" href="#">Home</a></li>
+                                <li><a href="#">Courses</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Web Design</a>
+                                            <ul>
+                                                <li><a href="#">HTML/CSS</a></li>
+                                                <li><a href="#">JavaScript</a></li>
+                                                <li><a href="#">Frameworks</a></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">Web Development</a>
+                                            <ul>
+                                                <li><a href="#">Frontend</a></li>
+                                                <li><a href="#">Backend</a></li>
+                                                <li><a href="#">Full Stack</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Graphic Design</a></li>
+                                        <li><a href="#">UI/UX Design</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Reviews</a></li>
+                            </ul>
+
+                        </nav>
+
+                        <!----- sign-up-btn ----->
+                        <div class="button btn-black">
+                            <a href="#">Sign Up</a>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </header>
